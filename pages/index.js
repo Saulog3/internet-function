968f96f1a8dbcad0d7ba8537b6193e780bab3f0f
Saulo Cunha <167145321+Saulog3@users.noreply.github.com>
2026-03-08 15:31:47 +0000

    configurar o prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
- function Home() {
-    return <h1>Eu fiz um site só para mostrar para minha cheirosa que eu consigo!</h1>
- }
- 
- export default Home;
+function Home() {
+  return (
+    <h1>Eu fiz um site só para mostrar para minha cheirosa que eu consigo!</h1>
+  );
+}
+
+export default Home;
